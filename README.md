f337e6700db4ae317a74c58872071decc3c0d67a
simranpreetmann <simranpreetmann@gmail.com>
2022-11-18 19:01:35 -0800

    This commit marks edits to the README.md file and the style of the app.R file was changed to tidyverse.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
+# Brief Discription 
+### What is this repository?
+This is the repository for Assignment B3 for STAT 545B. This assignment focuses on the creation of a Shiny app.
+  - [Assignment B3](https://stat545.stat.ubc.ca/assignments/assignment-b3/)
+  
+### What files are in this repository?
+This folder contains the following files:
+- [README.md](https://github.com/stat545ubc-2022/assignment-b3-simranpreetmann/blob/main/README.md) - Brief description of this folder
+- [app.R](https://github.com/stat545ubc-2022/assignment-b3-simranpreetmann/blob/main/app.R) - File with code for the Shiny app
+- [starbucks_data.csv](https://github.com/stat545ubc-2022/assignment-b3-simranpreetmann/blob/main/starbucks_data.csv) - Starbucks data that was used to create the Shiny app
+- [www](https://github.com/stat545ubc-2022/assignment-b3-simranpreetmann/tree/main/www) - A folder containing the `starbucks_logo.png` image that is used in the Shiny app
+
 
--- app.R
+++ app.R
@@ -8,8 +8,10 @@ library(fontawesome)
 starbucks <- read.csv("starbucks_data.csv", stringsAsFactors = FALSE)
 
 ui <- fluidPage(
-  tags$style(".note {color: #d62b1f;} #element {color: #d62b1f;}",
-             ".title {color: #00704A;} #element {color: #00704A;}"),
+  tags$style(
+    ".note {color: #d62b1f;} #element {color: #d62b1f;}",
+    ".title {color: #00704A;} #element {color: #00704A;}"
+  ),
   tags$style(HTML("
                   @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@800&display=swap');
                   .title {
@@ -31,17 +33,21 @@ ui <- fluidPage(
     sidebarPanel(
       img(src = "starbucks_logo.png", align = "center"),
       selectInput("sizeInput", "What size drink would you like?",
-                  choices = c("Short", "Tall", "Grande", "Venti", "Trenta", "Solo", "Doppio", "Triple", "Quad", "1 Scoop", "1 Shot")),
+        choices = c("Short", "Tall", "Grande", "Venti", "Trenta", "Solo", "Doppio", "Triple", "Quad", "1 Scoop", "1 Shot")
+      ),
       radioButtons("tempInput", "Would you like a hot or a cold drink?",
-                   choices = c("Hot", "Cold"),
-                   inline = TRUE),
+        choices = c("Hot", "Cold"),
+        inline = TRUE
+      ),
       checkboxGroupInput("milkInput", "What kind of milk would you like in your drink? (Suggestion: Don't get 2% or whole milk if you're lactose-intolerant)",
-                         choices = c("No Milk", "Non-Fat Milk", "Soy Milk", "Coconut Milk", "2% Milk", "Whole Milk"),
-                         selected = "Whole Milk"),
+        choices = c("No Milk", "Non-Fat Milk", "Soy Milk", "Coconut Milk", "2% Milk", "Whole Milk"),
+        selected = "Whole Milk"
+      ),
       radioButtons("whipInput", "Would you like whipped cream on your drink?",
-                   choices = c("Yes", "No"),
-                   selected = "No",
-                   inline = TRUE),
+        choices = c("Yes", "No"),
+        selected = "No",
+        inline = TRUE
+      ),
       submitButton("Show My Drink Options!")
     ),
     mainPanel(
@@ -60,109 +66,123 @@ server <- function(input, output) {
   output$sugarplot <- renderPlot({
     sugar_filtered <-
       starbucks %>%
-      filter(size == input$sizeInput,
-             temp == input$tempInput,
-             whip == input$whipInput) %>%
+      filter(
+        size == input$sizeInput,
+        temp == input$tempInput,
+        whip == input$whipInput
+      ) %>%
       filter(milk %in% input$milkInput)
     ggplot(sugar_filtered, aes(x = product_name)) +
       geom_col(aes(y = sugar_g), color = "#006241", fill = "#006241") +
       labs(x = "Name of Drink", y = "Sugar Content (g)") +
       scale_x_discrete(guide = guide_axis(angle = 90)) +
-      theme(axis.text.x = element_text(size = 9), axis.title = element_text(face="bold"))
+      theme(axis.text.x = element_text(size = 9), axis.title = element_text(face = "bold"))
   })
   output$caffeineplot <- renderPlot({
     caffeine_filtered <-
       starbucks %>%
-      filter(size == input$sizeInput,
-             temp == input$tempInput,
-             whip == input$whipInput) %>%
+      filter(
+        size == input$sizeInput,
+        temp == input$tempInput,
+        whip == input$whipInput
+      ) %>%
       filter(milk %in% input$milkInput)
     ggplot(caffeine_filtered, aes(x = product_name)) +
       geom_col(aes(y = caffeine_mg), color = "#fbbc05", fill = "#fbbc05") +
       labs(x = "Name of Drink", y = "Caffeine Content (mg)") +
       scale_x_discrete(guide = guide_axis(angle = 90)) +
-      theme(axis.text.x = element_text(size = 9), axis.title = element_text(face="bold"))
+      theme(axis.text.x = element_text(size = 9), axis.title = element_text(face = "bold"))
   })
   output$results <- DT::renderDataTable({
     filtered_table <-
       starbucks %>%
-      filter(size == input$sizeInput,
-             temp == input$tempInput,
-             whip == input$whipInput) %>%
+      filter(
+        size == input$sizeInput,
+        temp == input$tempInput,
+        whip == input$whipInput
+      ) %>%
       filter(milk %in% input$milkInput) %>%
-      rename("Name of Drink" = product_name,
-             "Size of Drink" = size,
-             "Type of Milk" = milk,
-             "Temperature" = temp,
-             "Whipped Cream?" = whip,
-             "Serving Size (mL)" = serv_size_m_l,
-             "Calories" = calories,
-             "Total Fat (g)" = total_fat_g,
-             "Saturated Fat (g)" = saturated_fat_g,
-             "Trans Fat (g)" = trans_fat_g,
-             "Cholesterol (mg)" = cholesterol_mg,
-             "Sodium (mg)" = sodium_mg,
-             "Total Carbohydrates (g)" = total_carbs_g,
-             "Fiber (g)" = fiber_g,
-             "Sugar (g)" = sugar_g,
-             "Caffeine (mg)" = caffeine_mg
-             )
+      rename(
+        "Name of Drink" = product_name,
+        "Size of Drink" = size,
+        "Type of Milk" = milk,
+        "Temperature" = temp,
+        "Whipped Cream?" = whip,
+        "Serving Size (mL)" = serv_size_m_l,
+        "Calories" = calories,
+        "Total Fat (g)" = total_fat_g,
+        "Saturated Fat (g)" = saturated_fat_g,
+        "Trans Fat (g)" = trans_fat_g,
+        "Cholesterol (mg)" = cholesterol_mg,
+        "Sodium (mg)" = sodium_mg,
+        "Total Carbohydrates (g)" = total_carbs_g,
+        "Fiber (g)" = fiber_g,
+        "Sugar (g)" = sugar_g,
+        "Caffeine (mg)" = caffeine_mg
+      )
     filtered_table
   })
   output$number_of_results <- renderText({
     filtered_table <-
       starbucks %>%
-      filter(size == input$sizeInput,
-             temp == input$tempInput,
-             whip == input$whipInput) %>%
+      filter(
+        size == input$sizeInput,
+        temp == input$tempInput,
+        whip == input$whipInput
+      ) %>%
       filter(milk %in% input$milkInput) %>%
-      rename("Name of Drink" = product_name,
-             "Size of Drink" = size,
-             "Type of Milk" = milk,
-             "Temperature" = temp,
-             "Whipped Cream?" = whip,
-             "Serving Size (mL)" = serv_size_m_l,
-             "Calories" = calories,
-             "Total Fat (g)" = total_fat_g,
-             "Saturated Fat (g)" = saturated_fat_g,
-             "Trans Fat (g)" = trans_fat_g,
-             "Cholesterol (mg)" = cholesterol_mg,
-             "Sodium (mg)" = sodium_mg,
-             "Total Carbohydrates (g)" = total_carbs_g,
-             "Fiber (g)" = fiber_g,
-             "Sugar (g)" = sugar_g,
-             "Caffeine (mg)" = caffeine_mg
+      rename(
+        "Name of Drink" = product_name,
+        "Size of Drink" = size,
+        "Type of Milk" = milk,
+        "Temperature" = temp,
+        "Whipped Cream?" = whip,
+        "Serving Size (mL)" = serv_size_m_l,
+        "Calories" = calories,
+        "Total Fat (g)" = total_fat_g,
+        "Saturated Fat (g)" = saturated_fat_g,
+        "Trans Fat (g)" = trans_fat_g,
+        "Cholesterol (mg)" = cholesterol_mg,
+        "Sodium (mg)" = sodium_mg,
+        "Total Carbohydrates (g)" = total_carbs_g,
+        "Fiber (g)" = fiber_g,
+        "Sugar (g)" = sugar_g,
+        "Caffeine (mg)" = caffeine_mg
       )
     number_rows <- nrow(filtered_table)
     paste("You can choose from", print(number_rows), "drink options!")
   })
   output$download_data <- downloadHandler(
     filename = function() {
-      paste("Starbucks Drink Options-", Sys.Date(), ".csv", sep="")
+      paste("Starbucks Drink Options-", Sys.Date(), ".csv", sep = "")
     },
     content = function(file) {
       filtered_table <-
         starbucks %>%
-        filter(size == input$sizeInput,
-               temp == input$tempInput,
-               whip == input$whipInput) %>%
+        filter(
+          size == input$sizeInput,
+          temp == input$tempInput,
+          whip == input$whipInput
+        ) %>%
         filter(milk %in% input$milkInput) %>%
-        rename("Name of Drink" = product_name,
-               "Size of Drink" = size,
-               "Type of Milk" = milk,
-               "Temperature" = temp,
-               "Whipped Cream?" = whip,
-               "Serving Size (mL)" = serv_size_m_l,
-               "Calories" = calories,
-               "Total Fat (g)" = total_fat_g,
-               "Saturated Fat (g)" = saturated_fat_g,
-               "Trans Fat (g)" = trans_fat_g,
-               "Cholesterol (mg)" = cholesterol_mg,
-               "Sodium (mg)" = sodium_mg,
-               "Total Carbohydrates (g)" = total_carbs_g,
-               "Fiber (g)" = fiber_g,
-               "Sugar (g)" = sugar_g,
-               "Caffeine (mg)" = caffeine_mg)
+        rename(
+          "Name of Drink" = product_name,
+          "Size of Drink" = size,
+          "Type of Milk" = milk,
+          "Temperature" = temp,
+          "Whipped Cream?" = whip,
+          "Serving Size (mL)" = serv_size_m_l,
+          "Calories" = calories,
+          "Total Fat (g)" = total_fat_g,
+          "Saturated Fat (g)" = saturated_fat_g,
+          "Trans Fat (g)" = trans_fat_g,
+          "Cholesterol (mg)" = cholesterol_mg,
+          "Sodium (mg)" = sodium_mg,
+          "Total Carbohydrates (g)" = total_carbs_g,
+          "Fiber (g)" = fiber_g,
+          "Sugar (g)" = sugar_g,
+          "Caffeine (mg)" = caffeine_mg
+        )
       write.csv(filtered_table, file)
     }
   )
